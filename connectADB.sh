be1c4f788e7f1a7fb4b6df752b85eafad5e3385b
Brooks J Rady <b.j.rady@gmail.com>
2016-10-27 15:55:11 -0600

    Moved code + Added Script

---
+++ connectADB.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Please connect the phone via USB to begin..."
+echo "Once the phone has been connected, please press Enter"
+read
+adb tcpip 5555
+echo "Please enter the ip address of the phone you would like to connect to: "
+read IP
+adb connect $IP:5555
