96ffa14a9a8429484a4c3ec4c03abd6d97ed3af3
Mike Harsch <m@mharsch.me>
2017-02-07 13:15:01 -0700

    add lightsensor led lightswitch

--- teamcode/tele/CalibrateLightSensors.java
+++ teamcode/tele/CalibrateLightSensors.java
@@ -21,6 +21,11 @@ public class CalibrateLightSensors extends LinearOpMode {
     @Override
     public void runOpMode() {
 
+	// these represent the on/off state of the light sensor leds
+	boolean lightSwitchPrev = false;
+	boolean lightSwitchCurr = false;
+	boolean lightSwitch = true;
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
@@ -35,6 +40,18 @@ public class CalibrateLightSensors extends LinearOpMode {
             telemetry.addData("Raw Light Readings: ", qwerty.debug("RawLight"));
             telemetry.addData("Light Readings: ", qwerty.debug("Light"));
             telemetry.addData("Light Threshold: ", qwerty.debug("LightThreshold"));
+
+            // edge trigger for x button lightswitch
+	    lightSwitchCurr = gamepad1.x;
+
+	    if ((lightSwitchCurr == true) && (lightSwitchCurr != lightSwitchPrev)) {
+                lightSwitch = !lightSwitch;
+		QWERTY.leftLS.enableLed(lightSwitch); // will this even work?
+		QWERTY.rightLS.enableLed(lightSwitch);
+	    }
+	    lightSwitchPrev = lightSwitchCurr;
+
+	    telemetry.addData("LEDs are", lightSwitch ? "On" : "Off");
 	    telemetry.update();
         }
     }
