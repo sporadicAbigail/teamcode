37988558306d1d62191488cbd32462d5f28e3bfa
Brooks J Rady <b.j.rady@gmail.com>
2017-04-28 16:52:33 +0000

    Simplify teamcode repo

--- syncDaemon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PWD=/home/ftc/pra_code
-
-while inotifywait -r -e modify,create,delete $PWD/teamcode; do
-    rsync -avz $PWD/teamcode/ $PWD/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode --delete
-done
